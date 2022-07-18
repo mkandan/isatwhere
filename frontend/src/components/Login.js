e0da3819196a32056aca6c1528e5673f3ef1d912
Mani Kandan <40218068+mkandan@users.noreply.github.com>
2022-07-18 15:44:51 -0700

    logged in / routes to /home. but /home problem still exists on refresh

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,8 +1,8 @@
 import { Link, useNavigate } from 'react-router-dom'
-import { useContext, useState } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import Alert from 'react-bootstrap/Alert'
 import { auth } from '../auth/firebase-config'
-import { signInWithPopup, TwitterAuthProvider } from "firebase/auth";
+import { signInWithPopup, TwitterAuthProvider, setPersistence, browserSessionPersistence } from "firebase/auth";
 import { UserAuthContext } from '../auth/UserAuthContext';
 import { axios } from 'axios'
 
@@ -13,20 +13,52 @@ const Login = () => {
     const [showErr, setShowErr] = useState(true);
     const navigate = useNavigate()
 
-    const { twitterLoggedIn, setTwitterLoggedIn, setoAuth_credential, setoAuth_token, setoAuth_secret } = useContext(UserAuthContext)
+    const { user, twitterLoggedIn, setTwitterLoggedIn, setoAuth_credential, setoAuth_token, setoAuth_secret } = useContext(UserAuthContext)
 
-    const logInWithTwitter2 = () => {
-        console.log('here login');
+    const [loggedIn, setLoggedIn] = useState(false)
+    // const [token, setToken] = useState('')
+
+    useEffect(() => {
+        auth.onAuthStateChanged((userCreds) => {
+            console.log('auth: ', auth);
+            console.log('user: ', user);
+            console.log('userCreds: ', userCreds);
+            userCreds.getIdToken()
+                .then((token) => {
+                    // console.log('token: ', token);
+                    // setToken(token)
+                    setLoggedIn(true)
+                    navigate('/home')
+                })
+        })
+    }, [])
+
+    setPersistence(auth, browserSessionPersistence)
+        .then(() => {
+            console.log('persistence set!');
+            return logInWithTwitter
+        })
+        .catch((error) => {
+            console.log(error.message);
+        })
+
+    const logInWithTwitter = () => {
+        console.log('twitter login button pressed');
         signInWithPopup(auth, providerTwitter)
             .then((result) => {
-                setTwitterLoggedIn(true)
+                if (result) {
+                    // setTwitterLoggedIn(true)
+                    setLoggedIn(true)
 
-                let cred = TwitterAuthProvider.credentialFromResult(result)
-                setoAuth_credential(cred)
-                setoAuth_token(cred.accessToken)
-                setoAuth_secret(cred.secret)
+                    let cred = TwitterAuthProvider.credentialFromResult(result)
+                    setoAuth_credential(cred)
+                    setoAuth_token(cred.accessToken)
+                    setoAuth_secret(cred.secret)
+                    // console.log('user LOGIN: ',user);
+                    console.log('auth.currentUser LOGIN: ', auth.currentUser);
 
-                navigate('/home')
+                    navigate('/home')
+                }
             })
             .catch((error) => {
                 setErr(error.message)
@@ -39,9 +71,10 @@ const Login = () => {
             {
                 err && showErr && <Alert variant='danger' dismissible onClose={() => setShowErr(false)}>{err}</Alert>
             }
-            <button onClick={logInWithTwitter2}>
+            <button onClick={logInWithTwitter}>
                 {/* <button onClick={logInWithPopupTwitter}> */}
-                Login with Twitter {twitterLoggedIn ? '✅' : '❌'}
+                {/* Login with Twitter {twitterLoggedIn ? '✅' : '❌'} */}
+                Login with Twitter {loggedIn ? '✅' : '❌'}
             </button>
             <p>
                 Don't have an account? Sign up <Link to={'/signup'}>here</Link>.
